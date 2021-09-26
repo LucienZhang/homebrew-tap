53aa785b436634e55712c518e601b09d9f812359
Lucien <lucienzhangzl@outlook.com>
2021-09-26 21:35:37 +0900

    update goto cli formula

--- Formula/goto-cli.rb
+++ Formula/goto-cli.rb
@@ -1,29 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class GotoCli < Formula
-    desc "An interactive command-line tool to manage your environments"
-    homepage "https://github.com/LucienZhang/goto"
-    url "https://github.com/LucienZhang/goto/releases/download/v0.1.0/goto-cli_0.1.0_Darwin_amd64.tar.gz"
-    sha256 "07e43ca34019243d3c38e1d00aae0d902d9fb3663a01bd23e8a883474e4607e8"
-    license "MIT"
+  desc "Goto is an interactive command-line tool to manage your environments"
+  homepage "https://github.com/LucienZhang/goto"
+  url "https://github.com/LucienZhang/goto.git",
+      tag:      "v1.0.0",
+      revision: "989e852275acd62f832365c58e605b724add281b"
+  license "MIT"
 
-    # depends_on "cmake" => :build
+  depends_on "go" => :build
 
-    def install
-      bin.install "goto-cli" => "goto"
-    end
-
-    test do
-      # `test do` will create, run in and delete a temporary directory.
-      #
-      # This test will fail and we won't accept that! For Homebrew/homebrew-core
-      # this will need to be a test that verifies the functionality of the
-      # software. Run the test with `brew test goto-cli`. Options passed
-      # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-      #
-      # The installed folder is not in the path, so use the entire path to any
-      # executables being tested: `system "#{bin}/program", "do", "something"`.
-      system "true"
-    end
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w -X github.com/LucienZhang/goto/configs.Version=#{version}"),
+                          "-o", bin/"goto", "main.go"
   end
+
+  test do
+    version_out = shell_output("#{bin}/goto --version")
+    assert_match "goto version " + version.to_s, version_out
+  end
+end
